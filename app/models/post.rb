5cfa0c257789400b7a938681aeb4b8601c1a1ac4
KonKinPak <tiewclub9@gmail.com>
2021-10-31 00:15:03 +0700

    Exclass9

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user 
   has_many :likes ,dependent: :destroy
   has_many :user_likes ,through: :likes, source: :user
+
+  def get_users_like_name
+   users = []
+   self.user_likes.each do |user|
+    users.push(user.name)
+   end
+   return users
+  end  
+
 end
